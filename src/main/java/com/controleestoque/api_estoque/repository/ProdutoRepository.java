741adfc054bcfc2cf88f50b33ca71afea7a1e205
LuisTorres2009 <luisrmt2009@gmail.com>
2025-11-30 22:25:14 -0300

    API Controle de Estoque

--- src/main/java/com/controleestoque/api_estoque/repository/ProdutoRepository.java
+++ src/main/java/com/controleestoque/api_estoque/repository/ProdutoRepository.java
@@ -12,7 +12,6 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long>{
     @Query("SELECT p FROM Produto p JOIN FETCH p.categoria WHERE p.id = :id")
     Optional<Produto> findByIdWithCategoria(Long id);
     
-    // Você também pode adicionar um para listar todos:
     @Query("SELECT p FROM Produto p JOIN FETCH p.categoria")
     List<Produto> findAllWithCategoria();
 }
